dbf8192ee2e14b70cc8d735529bce083587a3f5c
horney <jesu4274@ox.ac.uk>
2025-03-28 18:49:46 +0100

    Adding correcting for error in formatting

--- src/rdtfeeddown/plotting.py
+++ src/rdtfeeddown/plotting.py
@@ -3,13 +3,11 @@ import numpy as np
 from .analysis import polyfunction, calculate_avg_rdt_shift, arcBPMcheck, badBPMcheck
 
 def plot_BPM(BPM, data, rdt, rdt_plane, filename):
-
-    fig, (ax1, ax2) = plt.subplots(2,  1, sharey=False)
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharey=False)
     diffdata = data[BPM]['diffdata']
     fitdata = data[BPM]['fitdata']
-    xing = []
-    re = []
-    im = []
+    xing, re, im = [], [], []
+
     for x in range(len(diffdata)):
         xing.append(diffdata[x][0])
         re.append(diffdata[x][1])
@@ -18,22 +16,21 @@ def plot_BPM(BPM, data, rdt, rdt_plane, filename):
     xing = np.array(xing)
     re = np.array(re)
     im = np.array(im)
-    
+
     xing_min = np.min(xing)
     xing_max = np.max(xing)
     xing_ran = xing_max - xing_min
-    print(xing_min, xing_max, xing_ran)
     xfit = np.arange(xing_min, xing_max, xing_ran / 100.0)
     refit = polyfunction(xfit, fitdata[0][0], fitdata[0][1], fitdata[0][2])
     imfit = polyfunction(xfit, fitdata[3][0], fitdata[3][1], fitdata[3][2])
-    
-    ax1.set_ylabel(BPM + f' Re$f_{{{rdt_plane},{rdt}}}$')
-    ax1.set_xlabel('Knob trim')
+
+    ax1.set_ylabel(f"{BPM} Re$f_{{{rdt_plane},{rdt}}}$")
+    ax1.set_xlabel("Knob trim")
     ax1.plot(xfit, refit)
     ax1.plot(xing, re, 'ro')
 
-    ax2.set_ylabel(BPM + f' Im$f_{{{rdt_plane},{rdt}}}$')
-    ax2.set_xlabel('Knob trim')
+    ax2.set_ylabel(f"{BPM} Im$f_{{{rdt_plane},{rdt}}}$")
+    ax2.set_xlabel("Knob trim")
     ax2.plot(xfit, imfit)
     ax2.plot(xing, im, 'ro')
 
@@ -46,8 +43,8 @@ def plot_avg_rdt_shift(ax, data, rdt, rdt_plane, label_prefix):
     Plot the average RDT shift and standard deviation for given data on the provided axis.
     """
     xing, ampdat, stddat = calculate_avg_rdt_shift(data)
-    ax.set_ylabel(f'{label_prefix} sqrt($\\Delta$Re$f_{{{rdt_plane},{rdt}}}^2$+$\\Delta$Im$f_{{{rdt_plane},{rdt}}}^2$)')
-    ax.set_xlabel(f'{label_prefix} Knob trim')
+    ax.set_ylabel(f"{label_prefix} sqrt($\\Delta$Re$f_{{{rdt_plane},{rdt}}}^2$+$\\Delta$Im$f_{{{rdt_plane},{rdt}}}^2$)")
+    ax.set_xlabel(f"{label_prefix} Knob trim")
     ax.plot(xing, ampdat)
     ax.errorbar(xing, ampdat, yerr=stddat, fmt='ro')
 
@@ -77,13 +74,13 @@ def plot_RDTshifts(beam_data, rdt, rdt_plane, filename):
         dredkerr = np.array(dredkerr)
         dimdkerr = np.array(dimdkerr)
 
-        ax3.set_ylabel(f'{beam_label} dRe$f_{{{rdt_plane},{rdt}}}$/dknob')
-        ax3.set_xlabel(f'{beam_label} Knob trim')
+        ax3.set_ylabel(f"{beam_label} dRe$f_{{{rdt_plane},{rdt}}}$/dknob")
+        ax3.set_xlabel(f"{beam_label} Knob trim")
         ax3.plot(sdat, dredkdat)
         ax3.errorbar(sdat, dredkdat, yerr=dredkerr, fmt='ro')
 
-        ax5.set_ylabel(f'{beam_label} dIm$f_{{{rdt_plane},{rdt}}}$/dknob')
-        ax5.set_xlabel(f'{beam_label} Knob trim')
+        ax5.set_ylabel(f"{beam_label} dIm$f_{{{rdt_plane},{rdt}}}$/dknob")
+        ax5.set_xlabel(f"{beam_label} Knob trim")
         ax5.plot(sdat, dimdkdat)
         ax5.errorbar(sdat, dimdkdat, yerr=dimdkerr, fmt='ro')
 
@@ -99,14 +96,14 @@ def plot_RDT(beam_data, rdt, rdt_plane, filename):
     ax1, ax2, ax3, ax4, ax5, ax6 = axes.flatten()
 
     for i, (beam_label, data) in enumerate(beam_data.items()):
-        ax1.set_ylabel(f'{beam_label} $|f_{{{rdt_plane},{rdt}}}|$')
-        ax1.set_xlabel(f'{beam_label} Knob trim')
+        ax1.set_ylabel(f"{beam_label} $|f_{{{rdt_plane},{rdt}}}|$")
+        ax1.set_xlabel(f"{beam_label} Knob trim")
 
-        ax3.set_ylabel(f'{beam_label} $\Delta Re(f_{{{rdt_plane},{rdt}}})$')
-        ax3.set_xlabel(f'{beam_label} Knob trim')
+        ax3.set_ylabel(f"{beam_label} $\\Delta Re(f_{{{rdt_plane},{rdt}}})$")
+        ax3.set_xlabel(f"{beam_label} Knob trim")
 
-        ax5.set_ylabel(f'{beam_label} $\Delta Im(f_{{{rdt_plane},{rdt}}})$')
-        ax5.set_xlabel(f'{beam_label} Knob trim')
+        ax5.set_ylabel(f"{beam_label} $\\Delta Im(f_{{{rdt_plane},{rdt}}})$")
+        ax5.set_xlabel(f"{beam_label} Knob trim")
 
         xing = []
         for b in data.keys():
