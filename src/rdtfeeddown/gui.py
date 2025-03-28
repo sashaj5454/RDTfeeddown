1ef3f6cf450ef1063cef8c66c41f441a085ad4c3
Sasha Horney <sasha.horney@physics.ox.ac.uk>
2025-03-28 19:08:11 +0100

    Small changes

--- src/rdtfeeddown/gui.py
+++ src/rdtfeeddown/gui.py
@@ -330,7 +330,7 @@ class RDTFeeddownGUI(QMainWindow):
         rdt = self.rdt_entry.text()
         rdt_plane = self.rdt_plane_dropdown.currentText()  # Get selected value from dropdown
         knob = self.knob_entry.text()
-        output_path = self.default_output_path
+        output_path = self.default_output_path.text()
 
         # Validate inputs
         if not beam1_model and not beam2_model:
