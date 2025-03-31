e15b162d9471f93df73f9b56bd03285ad190707b
horney <jesu4274@ox.ac.uk>
2025-03-31 16:10:37 +0200

    Updating code to allow QtextCusor as meta-type

--- src/rdtfeeddown/__main__.py
+++ src/rdtfeeddown/__main__.py
@@ -1,8 +1,11 @@
 from .gui import RDTFeeddownGUI
+from PyQt5.QtCore import qRegisterMetaType
+from PyQt5.QtGui import QTextCursor
 from PyQt5.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication([])
+    qRegisterMetaType(QTextCursor, "QTextCursor")
     window = RDTFeeddownGUI()
     window.show()
     app.exec_()
