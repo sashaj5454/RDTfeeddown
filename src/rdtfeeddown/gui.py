430ba61dab089170bb8b21924b0af321b6e9aac7
Sasha Horney <sasha.horney@physics.ox.ac.uk>
2025-03-31 16:08:09 +0200

    Fixed some formatting issues

--- src/rdtfeeddown/gui.py
+++ src/rdtfeeddown/gui.py
@@ -10,6 +10,7 @@ from .utils import check_rdt, initialize_statetracker, rdt_to_order_and_type, ge
 from .analysis import write_RDTshifts, getrdt_omc3, fit_BPM
 import time  # Import time to get the current timestamp
 import re    # Import re for regex substitution
+from PyQt5.QtGui import QTextCursor
 
 class AnalysisWorker(QThread):
     finished = pyqtSignal()
@@ -195,12 +196,21 @@ class RDTFeeddownGUI(QMainWindow):
         # --- Parameters and Knob Group ---
         param_group = QtWidgets.QGroupBox("Parameters and Knob")
         param_layout = QVBoxLayout()
+        self.rdt_label = QLabel("RDT (in form of jklm):")
         param_layout.addWidget(self.rdt_label)       # "RDT (in form of jklm):"
+        self.rdt_entry = QLineEdit()
         param_layout.addWidget(self.rdt_entry)
+        self.rdt_plane_label = QLabel("RDT Plane:")
         param_layout.addWidget(self.rdt_plane_label)   # "RDT Plane:"
+        self.rdt_plane_dropdown = QtWidgets.QComboBox()
+        self.rdt_plane_dropdown.addItems(["x", "y"])
         param_layout.addWidget(self.rdt_plane_dropdown)
+        self.knob_label = QLabel("Knob:")
         param_layout.addWidget(self.knob_label)          # "Knob:"
+        self.knob_entry = QLineEdit("LHCBEAM/IP5-XING-H-MURAD")
         param_layout.addWidget(self.knob_entry)
+        self.validate_knob_button = QPushButton("Validate Knob")
+        self.validate_knob_button.clicked.connect(self.validate_knob_button_clicked)
         param_layout.addWidget(self.validate_knob_button)
         param_group.setLayout(param_layout)
         self.input_layout.addWidget(param_group)
